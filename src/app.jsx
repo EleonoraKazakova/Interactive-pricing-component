5da90090f6d615987727c51e33d1f4e86adc2f9d
Eleonora Kazakova <abei-2005@yandex.ru>
2021-04-20 17:44:48 +0200

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import './styles/app'
+
+export default function App() {
+  const pageviews = ["0", "10K", "50K", "100K", "1M"]
+  const prices = ['8.00', '12.00', '16.00', '24.00', '36.00']
+
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const changePrice = (event) => {
+    setSelectedIndex(event.target.value)
+  }
+
+  const percentColorRange = (selectedIndex * 100) / 4
+
+  return (
+    <div className='content'>
+      <div className='header'>
+        <p>Simple, traffic-based pricing</p>
+        <div className='subtitle'>
+        <p className='headerText'> Sign-up for our 30-day trial. No credit card required.</p>
+        </div>
+      </div>
+      <div className='component'>
+        <div className='price'>
+          <div className='headerText'>{pageviews[selectedIndex]} PAGEVIEWS</div>
+          <div className='headerText'> <div className='priceText'>${prices[selectedIndex]}</div> / per month</div>
+        </div>
+
+        <input
+          type="range"
+          min="0"
+          max="4"
+          value={selectedIndex} //current value of input
+          onChange={changePrice}
+          className='slider'
+          style={{ background: 'linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) ' + percentColorRange + '%, hsl(224, 65%, 95%) ' + percentColorRange + '%, hsl(224, 65%, 95%) 100%)' }}
+        />
+
+        <div className='billing'>
+          <p>Monthly Billing</p>
+          <span className='space'></span>
+          <div className='margin'>
+            <label className="switch">
+              <input type="checkbox" />
+              <span className="yearly-toggle"></span>
+            </label>
+          </div>
+          <span className='space'></span>
+          <p>Yearly Billing</p>
+          <span className='space'></span>
+          { screen.width > 1000 ? <p className='billingBackground'> 25% discount</p> : <p className='billingBackground'>- 25% </p>}
+          
+        </div>
+
+        <div className='line'></div>
+        <div className='lowerBlock'>
+          <div className='checkList'>
+            <div className='checkElement'>
+              <div className='check'></div><div>Unlimited websites</div>
+            </div>
+            <div className='checkElement'>
+              <div className='check'></div><div>100% data ownership</div>
+            </div>
+            <div className='checkElement'>
+              <div className='check'></div><div>Email reports</div>
+            </div>
+          </div>
+
+          <div className='button'>Start my trial</div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
